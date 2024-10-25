679a1a1243d37c3d9f8fe629b7316b9dcb58c11f
Yasmin Camargo <yfcamargo@hotmail.com>
2024-10-25 10:59:13 -0300

    commit 3

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -10,9 +10,9 @@ public class RentalService {
     private double pricePerDay;
     private double pricePerHour;
 
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(double pricePerDay, double pricePerHour, BrazilTaxService taxService) {
+    public RentalService(double pricePerDay, double pricePerHour, TaxService taxService) {
         this.pricePerDay = pricePerDay;
         this.pricePerHour = pricePerHour;
         this.taxService = taxService;
